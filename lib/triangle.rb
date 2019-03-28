8b9bf68229f382e45bc0e2136ffcf483c0356f6e
Daniel Schneider <daschne8@gmail.com>
2019-03-28 14:53:42 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,7 @@ class Triangle
     if @a<=0 || @b<=0 || @c<=0
       raise TriangleError
     end
-    if @a+@b<@c || @b+@c<@a || @c+@a<@b
+    if @a+@b<=@c || @b+@c<=@a || @c+@a<=@b
       raise TriangleError
     end
 
