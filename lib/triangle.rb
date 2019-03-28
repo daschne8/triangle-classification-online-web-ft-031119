55aba04d1d1153f483a44adcaa59b017843c2178
Daniel Schneider <daschne8@gmail.com>
2019-03-28 14:51:23 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,9 +6,10 @@ class Triangle
   end
 
   def kind
-    if @a==0 || @b==0 || @c==0
+    if @a<=0 || @b<=0 || @c<=0
       raise TriangleError
     end
+    
 
     if @a==@b && @b==@c
       @type = :equilateral
